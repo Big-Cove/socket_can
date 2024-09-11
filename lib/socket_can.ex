d64ac77c36bdeac51d23437c32cdf9da0ea8fa5e
Gus Workman <gus.a.workman@gmail.com>
2024-09-11 18:41:29 +0200

    initial commit

---
+++ lib/socket_can.ex
@@ -0,0 +1,18 @@
+defmodule SocketCan do
+  @moduledoc """
+  Documentation for `SocketCan`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SocketCan.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
